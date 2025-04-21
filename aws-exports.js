1429c51bd67e2feebb7a8d585629a0ff2145150e
armandocv <armandocv@gmail.com>
2025-04-21 15:34:47 -0500

    backend config

--- aws-exports.js
+++ aws-exports.js
@@ -1,10 +1,17 @@
-// Replace these values with your actual Cognito configuration
-const awsmobile = {
-  aws_project_region: 'us-east-1',
+// aws-exports.js
+const awsconfig = {
+  aws_project_region: 'us-west-2',
   aws_cognito_identity_pool_id: 'YOUR_IDENTITY_POOL_ID',
-  aws_cognito_region: 'us-east-1',
+  aws_cognito_region: 'us-west-2',
   aws_user_pools_id: 'YOUR_USER_POOL_ID',
   aws_user_pools_web_client_id: 'YOUR_APP_CLIENT_ID',
+  API: {
+    REST: {
+      'gamelift-api': {
+        endpoint: 'YOUR_API_GATEWAY_ENDPOINT',
+      },
+    },
+  },
 };
 
-export default awsmobile;
+export default awsconfig;
